4f7fa970c16c65d5a0430e066e48ddb95b560723
Chris Farley <cfarley95@gmail.com>
2018-05-19 02:22:55 -0400

    Start of phone authentication
    base code for phone authentication. Will test again in the morning.

--- Backend/pythonBackend.py
+++ Backend/pythonBackend.py
@@ -2,6 +2,10 @@ import stripe
 import sys
 import json
 from flask import Flask, render_template, request, jsonify
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from authy.api import AuthyApiClient
 
 
 stripe.api_key = "sk_test_RPqKcpfyn4rsbQ25aPiODSQ2"
@@ -37,4 +41,44 @@ def buyGas():
     )
 
     return "Gas order succeeded"
+    
+@app.route("/sendVerificationCode", methods=['GET', 'POST'])
+def sendCode():
+    authy_api = AuthyApiClient('8xpZf43L4paKxfL5ULKnNyFakUftTp0D')
+    data = request.json
+    #account_sid = "AC530922c72a72faee0034ae4e6e012f47"
+    #auth_token = "e67407672954476237b395e146d6d584"
+    #twilio_number= '+15862501425'
+    
+    phoneNumber = data["phoneNumber"]
+    authRequest = authy_api.phones.verification_start(phoneNumber, 1, via='sms')
+    if authRequest.content["success"] == "True":
+        return "Verification code sent"
+    else:
+        return "Error sending verification code"
+
+    
+
+
+@app.route("/checkVerificationCode", methods=['GET', 'POST'])
+def checkCode():
+    authy_api = AuthyApiClient('8xpZf43L4paKxfL5ULKnNyFakUftTp0D')
+    data = request.json
+    phoneNumber = data["phoneNumber"]
+    verificationCode = data["verificationCode"]
+    authCheck = authy_api.phones.verification_check(phoneNumber, 1, verificationCode)
+    if authCheck.ok() == "True":
+        return "Authentication successful"
+    else:
+        return "Authentication error."
+
+
+
+
+
+
+
+
+
+
     
