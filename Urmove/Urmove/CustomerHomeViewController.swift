db7452e5535d81fc641269e269d17f89a588e9cc
Chris Farley <christian@Christians-Mac-mini.local>
2018-05-22 02:40:00 -0400

    Google Maps/address search Implementation
    Google maps API implemented. Address search also implemented. Will
    finish additional features tomorrow

--- Urmove/Urmove/CustomerHomeViewController.swift
+++ Urmove/Urmove/CustomerHomeViewController.swift
@@ -7,13 +7,83 @@
 //
 
 import UIKit
+import GoogleMaps
+import CoreLocation
+import GooglePlaces
+class CustomerHomeViewController: UIViewController,CLLocationManagerDelegate,GMSAutocompleteResultsViewControllerDelegate  {
+    var userLocation: CLLocation?
+    var locationManager: CLLocationManager = CLLocationManager()
+    var resultsViewController: GMSAutocompleteResultsViewController?
+    var searchController: UISearchController?
+    var resultView: UITextView?
 
-class CustomerHomeViewController: UIViewController {
-
+    @IBOutlet weak var mapView: GMSMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        resultsViewController = GMSAutocompleteResultsViewController()
+        resultsViewController?.delegate = self
+        
+        searchController = UISearchController(searchResultsController: resultsViewController)
+        searchController?.searchResultsUpdater = resultsViewController
+        let subView = UIView(frame: CGRect(x: 0, y: 65.0, width: 350.0, height: 45.0))
+        subView.addSubview((searchController?.searchBar)!)
+        view.addSubview(subView)
+        searchController?.searchBar.sizeToFit()
+        searchController?.hidesNavigationBarDuringPresentation = false
+        
+        // When UISearchController presents the results view, present it in
+        // this view controller, not one further up the chain.
+        definesPresentationContext = true
+
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        locationManager.requestLocation()
+        
+        userLocation = locationManager.location
+        
+        let userLat = userLocation?.coordinate.latitude
+        let userLong = userLocation?.coordinate.longitude
+        let camera = GMSCameraPosition.camera(withLatitude: Double(userLat ?? 34.0616372), longitude: Double(userLong ?? -84.6237319), zoom: 10.0)
+        mapView.camera = camera
+        mapView.isMyLocationEnabled = true
+        if let mylocation = mapView.myLocation {
+            print("User's location: \(mylocation)")
+            
+        } else {
+            print("User's location is unknown")
+        }
 
         // Do any additional setup after loading the view.
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("update")
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+    }
+    func resultsController(_ resultsController: GMSAutocompleteResultsViewController,
+                           didAutocompleteWith place: GMSPlace) {
+        searchController?.isActive = false
+        // Do something with the selected place.
+        print("Place name: \(place.name)")
+        print("Place address: \(place.formattedAddress)")
+        print("Place attributions: \(place.attributions)")
+    }
+    
+    func resultsController(_ resultsController: GMSAutocompleteResultsViewController,
+                           didFailAutocompleteWithError error: Error){
+        // TODO: handle the error.
+        print("Error: ", error.localizedDescription)
+    }
+    
+    // Turn the network activity indicator on and off again.
+    func didRequestAutocompletePredictions(forResultsController resultsController: GMSAutocompleteResultsViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    func didUpdateAutocompletePredictions(forResultsController resultsController: GMSAutocompleteResultsViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,3 +103,4 @@ class CustomerHomeViewController: UIViewController {
     */
 
 }
+
